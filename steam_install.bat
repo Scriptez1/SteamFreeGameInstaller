76c2d5a60230b157cfebea8a88fb87def00ac70f
Script.ez <96830819+Scriptez1@users.noreply.github.com>
2025-04-20 17:37:50 +0300

    added file

---
+++ steam_install.bat
@@ -0,0 +1,170 @@
+@echo off
+title Steam Game Installer Deluxe - By Scriptez1
+color 0A
+mode 100,30
+
+echo ============================================================================
+echo       STEAM OYUN YUKLEYICI v1.0 - STEAM GAME INSTALLER v1.0
+echo ============================================================================
+echo.
+timeout /t 2 >nul
+
+echo - [1/38] 8-Bit Bayonetta
+start steam://install/567090
+timeout /t 2 >nul
+
+echo - [2/38] Age of Empires Online - Servers Down
+start steam://install/105430
+timeout /t 2 >nul
+
+echo - [3/38] Arcane Saga Online
+start steam://install/238110
+timeout /t 2 >nul
+
+echo - [4/38] Arctic Combat
+start steam://install/212370
+timeout /t 2 >nul
+
+echo - [5/38] Arma 2: Free Version
+start steam://install/107400
+timeout /t 2 >nul
+
+echo - [6/38] Battle for Graxia
+start steam://install/90530
+timeout /t 2 >nul
+
+echo - [7/38] Brawl Busters
+start steam://install/109410
+timeout /t 2 >nul
+
+echo - [8/38] Bullet Run
+start steam://install/211880
+timeout /t 2 >nul
+
+echo - [9/38] Codename Gordon
+start steam://install/92
+timeout /t 2 >nul
+
+echo - [10/38] District 187
+start steam://install/221080
+timeout /t 2 >nul
+
+echo - [11/38] Dungeon Fighter Online
+start steam://install/212220
+timeout /t 2 >nul
+
+echo - [12/38] F.E.A.R. Online - Servers Down
+start steam://install/223650
+timeout /t 2 >nul
+
+echo - [13/38] Fnaf World
+start steam://install/427920
+timeout /t 2 >nul
+
+echo - [14/38] Haunted Memories
+start steam://install/241640
+timeout /t 2 >nul
+
+echo - [15/38] Maple Story (US-Version)
+start steam://install/216150
+timeout /t 2 >nul
+
+echo - [16/38] Pandora Saga
+start steam://install/106010
+timeout /t 2 >nul
+
+echo - [17/38] Renaissance Heroes
+start steam://install/221790
+timeout /t 2 >nul
+
+echo - [18/38] Rusty Hearts
+start steam://install/36630
+timeout /t 2 >nul
+
+echo - [19/38] Spacewar
+start steam://install/480
+timeout /t 2 >nul
+
+echo - [20/38] TERA
+start steam://install/389300
+timeout /t 2 >nul
+
+echo - [21/38] TERA EU
+start steam://install/323370
+timeout /t 2 >nul
+
+echo - [22/38] Vanguard: Saga of Heroes F2P
+start steam://install/218210
+timeout /t 2 >nul
+
+echo - [23/38] Wizardry Online
+start steam://install/221360
+timeout /t 2 >nul
+
+echo - [24/38] America's Army 3
+start steam://install/13140
+timeout /t 2 >nul
+
+echo - [25/38] Smashball
+start steam://install/17730
+timeout /t 2 >nul
+
+echo - [26/38] TrackMania Nations Forever
+start steam://install/11020
+timeout /t 2 >nul
+
+echo - [27/38] Peggle Extreme
+start steam://install/3483
+timeout /t 2 >nul
+
+echo - [28/38] Forge
+start steam://install/223390
+timeout /t 2 >nul
+
+echo - [29/38] Warface
+start steam://install/291480
+timeout /t 2 >nul
+
+echo - [30/38] Vindictus
+start steam://install/212160
+timeout /t 2 >nul
+
+echo - [31/38] Dirty Bomb
+start steam://install/333930
+timeout /t 2 >nul
+
+echo - [32/38] Dragon Nest
+start steam://install/11610
+timeout /t 2 >nul
+
+echo - [33/38] Metro Conflict
+start steam://install/356640
+timeout /t 2 >nul
+
+echo - [34/38] Brick-Force (US)
+start steam://install/272490
+timeout /t 2 >nul
+
+echo - [35/38] Fiesta Online NA
+start steam://install/300970
+timeout /t 2 >nul
+
+echo - [36/38] Atlantica Online
+start steam://install/212240
+timeout /t 2 >nul
+
+echo - [37/38] Sin of a Dark Age
+start steam://install/223390
+timeout /t 2 >nul
+
+echo - [38/38] DC Universe Online
+start steam://install/24200
+timeout /t 2 >nul
+
+color 0A
+cls
+echo =============================================================================
+echo |-  TÜM OYUNLAR YÜKLENDİ! KRAL, AFİYETLE KUR  - SUCCSUCCESSFULLY COMPLETED -|
+echo |-   Steam Library'ni kontrol et, şov başlasın! - Check your Steam Library -|
+echo =============================================================================
+pause
