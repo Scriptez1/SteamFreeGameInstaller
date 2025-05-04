abda2a3b89de6f085820029527fc4b7dbffb5da4
Script.ez <96830819+Scriptez1@users.noreply.github.com>
2025-05-04 15:25:14 +0300

    Update steam_install.bat

--- steam_install.bat
+++ steam_install.bat
@@ -4,215 +4,139 @@ color 0A
 mode 100,30
 
 echo ============================================================================
-echo       STEAM OYUN YUKLEYICI v1.1 - STEAM GAME INSTALLER v1.1
+echo       STEAM OYUN YUKLEYICI v1.2 - STEAM GAME INSTALLER v1.2
 echo ============================================================================
 echo.
 timeout /t 2 >nul
 
-echo - [1/50] 8-Bit Bayonetta
+echo - [1/31] 8-Bit Bayonetta
 start steam://install/567090
 timeout /t 2 >nul
 
-echo - [2/50] Age of Empires Online - Servers Down
+echo - [2/31] Age of Empires Online - Servers Down
 start steam://install/105430
 timeout /t 2 >nul
 
-echo - [3/50] Arcane Saga Online
+echo - [3/31] Arcane Saga Online
 start steam://install/238110
 timeout /t 2 >nul
 
-echo - [4/50] Arctic Combat
+echo - [4/31] Arctic Combat
 start steam://install/212370
 timeout /t 2 >nul
 
-echo - [5/50] Arma 2: Free Version
+echo - [5/31] Arma 2: Free Version
 start steam://install/107400
 timeout /t 2 >nul
 
-echo - [6/50] Battle for Graxia
+echo - [6/31] Battle for Graxia
 start steam://install/90530
 timeout /t 2 >nul
 
-echo - [7/50] Brawl Busters
+echo - [7/31] Brawl Busters
 start steam://install/109410
 timeout /t 2 >nul
 
-echo - [8/50] Bullet Run
-start steam://install/211880
-timeout /t 2 >nul
-
-echo - [9/50] Codename Gordon
+echo - [8/31] Codename Gordon
 start steam://install/92
 timeout /t 2 >nul
 
-echo - [10/50] District 187
+echo - [9/31] District 187
 start steam://install/221080
 timeout /t 2 >nul
 
-echo - [11/50] Dungeon Fighter Online
+echo - [10/31] Dungeon Fighter Online
 start steam://install/212220
 timeout /t 2 >nul
 
-echo - [12/50] F.E.A.R. Online - Servers Down
+echo - [11/31] F.E.A.R. Online - Servers Down
 start steam://install/223650
 timeout /t 2 >nul
 
-echo - [13/50] Fnaf World
-start steam://install/427920
-timeout /t 2 >nul
-
-echo - [14/50] Haunted Memories
+echo - [12/31] Haunted Memories
 start steam://install/241640
 timeout /t 2 >nul
 
-echo - [15/50] Maple Story (US-Version)
+echo - [13/31] Maple Story (US-Version)
 start steam://install/216150
 timeout /t 2 >nul
 
-echo - [16/50] Pandora Saga
+echo - [14/31] Pandora Saga
 start steam://install/106010
 timeout /t 2 >nul
 
-echo - [17/50] Renaissance Heroes
+echo - [15/31] Renaissance Heroes
 start steam://install/221790
 timeout /t 2 >nul
 
-echo - [18/50] Rusty Hearts
+echo - [16/31] Rusty Hearts
 start steam://install/36630
 timeout /t 2 >nul
 
-echo - [19/50] Spacewar
+echo - [17/31] Spacewar
 start steam://install/480
 timeout /t 2 >nul
 
-echo - [20/50] TERA
-start steam://install/389300
-timeout /t 2 >nul
-
-echo - [21/50] TERA EU
-start steam://install/323370
-timeout /t 2 >nul
-
-echo - [22/50] Vanguard: Saga of Heroes F2P
+echo - [18/31] Vanguard: Saga of Heroes F2P
 start steam://install/218210
 timeout /t 2 >nul
 
-echo - [23/50] Wizardry Online
-start steam://install/221360
-timeout /t 2 >nul
-
-echo - [24/50] America's Army 3
+echo - [19/31] America's Army 3
 start steam://install/13140
 timeout /t 2 >nul
 
-echo - [25/50] Smashball
+echo - [20/31] Smashball
 start steam://install/17730
 timeout /t 2 >nul
 
-echo - [26/50] TrackMania Nations Forever
+echo - [21/31] TrackMania Nations Forever
 start steam://install/11020
 timeout /t 2 >nul
 
-echo - [27/50] Peggle Extreme
+echo - [22/31] Peggle Extreme
 start steam://install/3483
 timeout /t 2 >nul
 
-echo - [28/50] Forge
+echo - [23/31] Forge
 start steam://install/223390
 timeout /t 2 >nul
 
-echo - [29/50] Warface
+echo - [24/31] Warface
 start steam://install/291480
 timeout /t 2 >nul
 
-echo - [30/50] Vindictus
+echo - [25/31] Vindictus
 start steam://install/212160
 timeout /t 2 >nul
 
-echo - [31/50] Dirty Bomb
+echo - [26/31] Dirty Bomb
 start steam://install/333930
 timeout /t 2 >nul
 
-echo - [32/50] Dragon Nest
-start steam://install/11610
-timeout /t 2 >nul
-
-echo - [33/50] Metro Conflict
+echo - [27/31] Metro Conflict
 start steam://install/356640
 timeout /t 2 >nul
 
-echo - [34/50] Brick-Force (US)
-start steam://install/272490
-timeout /t 2 >nul
-
-echo - [35/50] Fiesta Online NA
+echo - [28/31] Fiesta Online NA
 start steam://install/300970
 timeout /t 2 >nul
 
-echo - [36/50] Atlantica Online
+echo - [29/31] Atlantica Online
 start steam://install/212240
 timeout /t 2 >nul
 
-echo - [37/50] Sin of a Dark Age
-start steam://install/223390
-timeout /t 2 >nul
-
-echo - [38/50] DC Universe Online
+echo - [30/31] DC Universe Online
 start steam://install/24200
 timeout /t 2 >nul
 
-echo - [39/50] PES 2016 myClub
-start steam://install/407250
-timeout /t 2 >nul
-
-echo - [40/50] Streets Of Kamurocho
-start steam://install/1368430
-timeout /t 2 >nul
-
-echo - [41/50] Golden Axed: A Cancelled Prototype
-start steam://install/1368460
-timeout /t 2 >nul
-
-echo - [42/50] Endless Zone
-start steam://install/1368450
-timeout /t 2 >nul
-
-echo - [43/50] Armor of Heroes
-start steam://install/1368440
-timeout /t 2 >nul
-
-echo - [44/50] SEGA Bass Fishing
-start steam://install/71240
-timeout /t 2 >nul
-
-echo - [45/50] Alpha Prime
-start steam://install/2590
-timeout /t 2 >nul
-
-echo - [46/50] Ricochet
-start steam://install/60
-timeout /t 2 >nul
-
-echo - [47/50] Tom Clancy's EndWar
-start steam://install/15620
-timeout /t 2 >nul
-
-echo - [48/50] RaceRoom Racing Experience
-start steam://install/211500
-timeout /t 2 >nul
-
-echo - [49/50] War Inc. Battlezone
-start steam://install/97100
-timeout /t 2 >nul
-
-echo - [50/50] Nosgoth
-start steam://install/200110
+echo - [31/31] The Mighty Quest For Epic Loot
+start steam://install/239220
 timeout /t 2 >nul
 
 color 0A
 cls
 echo =============================================================================
-echo |-  TÜM OYUNLAR YÜKLENDİ! KRAL, AFİYETLE KUR  - SUCCESSFULLY COMPLETED -|
-echo |-   Steam Library'ni kontrol et, şov başlasın! - Check your Steam Library -|
+echo  -  TUM OYUNLAR YUKLENDI! KRAL, AFIYETLE KUR  - SUCCESSFULLY COMPLETED -
+echo  -   Steam Library'ni kontrol et, sov baslasin! - Check your Steam Library -
 echo =============================================================================
 pause
